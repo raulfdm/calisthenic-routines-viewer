6f26c5855753aebfbb1742eb8f7e64aed309f02e
Raul Melo <melo.raulf@gmail.com>
2018-08-19 23:12:40 +0200

    Style MainPhotoButton component

---
+++ src/components/MainPhotoButton/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  Dimensions,
+  TouchableOpacity
+} from 'react-native';
+
+export default ({ text, photo }) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => console.warn('hehe')}>
+        <ImageBackground source={photo} style={styles.photo}>
+          <Text style={styles.text}>{text}</Text>
+        </ImageBackground>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexBasis: Dimensions.get('screen').width / 2,
+    height: Dimensions.get('screen').height / 2
+  },
+  text: {
+    color: '#fff',
+    fontFamily: 'Roboto',
+    fontWeight: '400',
+    fontSize: 28
+  },
+  photo: {
+    width: Dimensions.get('screen').width / 2,
+    height: '100%',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
