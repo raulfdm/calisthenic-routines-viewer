eefae97964ce836ab3660ba67c6907b6dde8fb4d
Raul Melo <melo.raulf@gmail.com>
2018-08-19 18:12:33 +0200

    Setup react-native project

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class App extends Component {
-  render() {
-    return <React.Fragment>{this.props.children}</React.Fragment>;
-  }
-}
-
-export default App;
