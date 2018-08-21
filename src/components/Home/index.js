e2a37bfc9e05523ca78140b8b8f260aeb99e5253
Raul Melo <melo.raulf@gmail.com>
2018-08-21 20:33:18 +0200

    Fix requires

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,22 +7,26 @@ export default () => {
     {
       id: 4,
       text: 'Warm Up',
-      photo: require('../../images/background/warmup.png')
+      photo: require('../../images/background/warmup.png'),
+      path: 'warmup'
     },
     {
       id: 1,
       text: 'Beginner',
-      photo: require('../../images/background/beginner.png')
+      photo: require('../../images/background/beginner.png'),
+      path: 'beginner'
     },
     {
       id: 2,
       text: 'Intermediate',
-      photo: require('../../images/background/intermediate.png')
+      photo: require('../../images/background/intermediate.png'),
+      path: 'intermediate'
     },
     {
       id: 3,
       text: 'Advanced',
-      photo: require('../../images/background/advanced.png')
+      photo: require('../../images/background/advanced.png'),
+      path: 'advanced'
     }
   ];
   return (
