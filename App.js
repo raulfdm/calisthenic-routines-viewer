9e9be9e095e8149e8577e7fc41d8282be7ffa682
Raul Melo <melo.raulf@gmail.com>
2018-08-21 20:33:26 +0200

    Start to use react-router

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import { NativeRouter, Route } from 'react-router-native';
+import { NativeRouter, Route, Switch } from 'react-router-native';
 import Home from './src/components/Home';
+import RoutinePage from './src/components/RoutinePage';
+import routines from './src/globals/routines';
 
 export default class App extends React.Component {
   render() {
     return (
       <NativeRouter>
-        <Route exact path="/" component={Home} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route
+            exact
+            path="routine/:name"
+            render={props => {
+              const { name } = props.match.params;
+              const nextRoutine = routines[name];
+              console.log(nextRoutine);
+              return <RoutinePage routine={nextRoutine} {...props} />;
+            }}
+          />
+        </Switch>
       </NativeRouter>
     );
   }
