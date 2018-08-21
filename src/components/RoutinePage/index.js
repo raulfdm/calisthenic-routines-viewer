273ff20f8c7ca25aee11212601ece0e9ac08a0cd
Raul Melo <melo.raulf@gmail.com>
2018-08-21 20:32:58 +0200

    Create RoutinePage Component

---
+++ src/components/RoutinePage/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default () => {
+  return (
+    <View>
+      Oieee Oieee Oieee Oieee Oieee Oieee Oieee Oieee Oieee Oieee Oieee Oieee
+      Oieee Oieee Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+      voluptate placeat dolores reprehenderit minus nemo autem labore veniam
+      maxime esse, voluptas non beatae! Praesentium, esse ab! Quaerat quo est
+      maxime?
+    </View>
+  );
+};
