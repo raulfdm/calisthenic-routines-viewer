c36ca9a5e7196962514994ffa453d80ed8a97b09
Raul Melo <melo.raulf@gmail.com>
2018-08-22 21:23:37 +0200

    Move Home and Routines to "screens" folder

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,31 +1,31 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import MainPhotoButton from '../MainPhotoButton';
+import MainPhotoButton from 'components/MainPhotoButton';
 
 export default () => {
   const options = [
     {
       id: 4,
       text: 'Warm Up',
-      photo: require('../../images/background/warmup.png'),
+      photo: require('images/background/warmup.png'),
       path: 'warmup'
     },
     {
       id: 1,
       text: 'Beginner',
-      photo: require('../../images/background/beginner.png'),
+      photo: require('images/background/beginner.png'),
       path: 'beginner'
     },
     {
       id: 2,
       text: 'Intermediate',
-      photo: require('../../images/background/intermediate.png'),
+      photo: require('images/background/intermediate.png'),
       path: 'intermediate'
     },
     {
       id: 3,
       text: 'Advanced',
-      photo: require('../../images/background/advanced.png'),
+      photo: require('images/background/advanced.png'),
       path: 'advanced'
     }
   ];
